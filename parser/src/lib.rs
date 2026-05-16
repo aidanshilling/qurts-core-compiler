7dee26543a85626fe817754ac87c59c160cc289d
Aidan Shilling <aidanshilling@disroot.org>
2026-05-16 16:28:39 +0100

    Add basic lifetime-parsing tests

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -11,11 +11,23 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let successful_parse = QurtsParser::parse(Rule::program, "fn beans() -> bool {}");
+        let successful_parse = QurtsParser::parse(Rule::program, "fn test() -> bool {}");
         println!("{:?}", successful_parse);
         let unsuccessful_parse = QurtsParser::parse(Rule::program, "func 1-2 () {}");
         println!("{:?}", unsuccessful_parse);
         assert!(successful_parse.is_ok());
         assert!(unsuccessful_parse.is_err());
+    }
+
+    #[test]
+    fn single_lifetime() {
+        let result = QurtsParser::parse(Rule::program, "fn test<'a>() -> bool {}");
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn constrained_lifetime() {
+        let result = QurtsParser::parse(Rule::program, "fn test<'b != '0>() -> qbit {}");
+        assert!(result.is_ok());
     }
 }
